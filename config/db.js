c1d8d0374433cfb5aba52ddb6609bd30ba8f75e2
rohit-pluzbuzz <rohit.pluzbuzz@gmail.com>
2025-11-15 23:55:44 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const db = mysql.createPool({
-  host: "mysql-2d196fdd-vivana.f.aivencloud.com", // ✅ only hostname here
-  port: 16963, // ✅ add port separately
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "devshback.cxkq8k2a6xgg.ap-south-1.rds.amazonaws.com", // ✅ only hostname here
+  port: 3306, // ✅ add port separately
+  user: "admin",
+  password: "adminpass",
+  database: "dev.sh.back",
   waitForConnections: true,
   connectionLimit: 4,
   queueLimit: 0,

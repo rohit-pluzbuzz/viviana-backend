c6a9edf69522035ad0a65cb9dd2bf426f0d0ba76
rohit-pluzbuzz <rohit.pluzbuzz@gmail.com>
2025-11-16 00:01:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json()); // 👈 This parses incoming JSON body
 
 app.use(cors({
-  origin: ['http://localhost:5174', 'http://localhost:5173'],
+  origin: ['https://viviana-admin.onrender.com', 'https://viviana-frontend.onrender.com'],
   credentials: true   // allow cookies and auth headers
 }));
 
@@ -31,7 +31,7 @@ app.use("/api/upload", uploadRoutes); // <-- this line is important
 //  auth routes
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
